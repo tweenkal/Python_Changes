62c250e91ec068201dcea31d9262d29976a492bb
tweenkal <twinklepansuriya4124@gmail.com>
2026-02-05 22:42:42 +0530

    Update docstring and add book_ids field

--- models/models/book_category.py
+++ models/models/book_category.py
@@ -2,8 +2,11 @@
 from odoo import models, fields
 
 class BookCategory(models.Model):
-    """Model to store book categories."""
+    """
+    Model for storing Book Categories.
+    """
     _name = 'book.category'
     _description = 'Book Category'
 
     category_name = fields.Char(string='Category Name', required=True)
+    book_ids = fields.One2many('library.book', 'category_id', string='Books')
