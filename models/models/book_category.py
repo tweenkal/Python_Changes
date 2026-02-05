63aab23f0eac6c9ee22909c71faba8c6989f167a
tweenkal <twinklepansuriya4124@gmail.com>
2026-02-05 23:08:35 +0530

    Update book_category.py

--- models/models/book_category.py
+++ models/models/book_category.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 class BookCategory(models.Model):
-    """
-    Model for storing Book Categories.
-    """
     _name = 'book.category'
     _description = 'Book Category'
 
     category_name = fields.Char(string='Category Name', required=True)
-    book_ids = fields.One2many('library.book', 'category_id', string='Books')
