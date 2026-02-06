1c987c1f7b7be09879af2e68726c597458a6d922
tweenkal <twinklepansuriya4124@gmail.com>
2026-02-06 08:00:19 +0530

    Update book_category.py

--- models/models/book_category.py
+++ models/models/book_category.py
@@ -1,6 +1,15 @@
 from odoo import models, fields
 
 class BookCategory(models.Model):
+    """
+    Book Category Model
+
+    This model is used to manage different categories of books
+    in the Library Management system.
+
+    Fields:
+        category_name (Char): Name of the book category.
+    """
     _name = 'book.category'
     _description = 'Book Category'
 
