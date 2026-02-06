642d594de1b48ab384c6d31f6d86ed5ea6052f23
tweenkal <twinklepansuriya4124@gmail.com>
2026-02-06 08:14:39 +0530

    Update menifest.py

--- menifest.py
+++ menifest.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
+
 {
     'name': 'AK Library Management',
-    'version': '1.0',
+    'version': '19.0.1.0.0',
     'summary': 'Manage library books and categories',
+    'description': """
+Library Management module for Odoo.
+- Create and manage books.
+- Organize books by categories.
+    """,
     'category': 'Management',
     'depends': ['base', 'web'],
     'author': 'Tweenkal',
+    'website': 'https://www.aktivsoftware.com',
     'data': [
-        'security/ir.model.access.csv',
-        'views/library_book_views.xml',
-        'views/book_category_views.xml',
-        'views/library_menu_views.xml',
+        'security/ir.model.access.csv',          # access rules first
+        'views/library_books_views.xml',         # book views
+        'views/book_categories_views.xml',       # category views
+        'views/library_menus_views.xml',         # menus
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
     'license': 'AGPL-3',
+    'installable': True,     # module can be installed
+    'auto_install': False,   # module will NOT install automatically
+    'application': True,     # module shows in Apps dashboard
 }
