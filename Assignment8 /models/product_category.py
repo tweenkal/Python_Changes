5611f1c66c72fc238551f789992e5da34a9c90f4
tweenkal <twinklepansuriya4124@gmail.com>
2026-03-05 21:46:28 +0530

    Update product_category.py

--- Assignment8 /models/product_category.py
+++ Assignment8 /models/product_category.py
@@ -4,3 +4,14 @@ class ProductCategory(models.Model):
     _inherit = "product.category"
 
     book_categ_id = fields.Many2one("book.category", string="Book Category")
+
+    def action_open_book_category(self):
+
+    self.ensure_one()
+
+    return {
+        'type': 'ir.actions.act_window',
+        'res_model': 'book.category',
+        'view_mode': 'form',
+        'res_id': self.book_categ_id.id
+    }
